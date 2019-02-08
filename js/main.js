8663eebd6cff74290151b23b281fb741ad3c6f81
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-02-08 16:54:23 +0000

    task 1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-$('[data-toggle="refresh-list"]').on('click', function(){
-  alert("When this is clicked, refresh the images on the site.");
+var images = []
 
+$('[data-toggle="refresh-list"]').on("click", function(){
   var target = $($(this).data('target'));
   var source = $(this).data('source')
 
-  console.log( "Tag to refresh:", target );
-  console.log( "Source of images:", source );
-
-  // Not sure what to do with those values, I think I can use: https://api.jquery.com/jQuery.ajax/ 
+ // Not sure what to do with those values, I think I can use: https://api.jquery.com/jQuery.ajax/ 
   $.ajax({
     url: source
   }).done(function( data ) {
-    debugger;
-
-    // I think I can use _.shuffle(data) to shuffle the array I get here.
-    // Then update those 3 images somehow...
-    console.log(data)
-    console.log(target.find('.card')[0])
+    images = _.shuffle(JSON.parse(data));
+    target.find('.card').each(function( index ){
+      $(this).find('img').attr('src', images[index].url);
+      $(this).find('.card-text').text(images[index].description);
+    });
   });
 });
